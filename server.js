023a5e7a18691e2c844c50d30e1e5af62ace4902
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-02-17 13:11:38 -0600

    set up server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const db = require("./models");
+const mongoose = require("mongoose");
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"))
+  })
+  
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"))
+  })
