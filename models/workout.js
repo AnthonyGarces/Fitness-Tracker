746c5750786b4e1613ba3bef33664682798c3d89
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-02-17 13:20:35 -0600

    constructed model for workouts

---
+++ models/workout.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userWorkout = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+
+    exercises: [{
+        type: {type: String},
+        name: {type: String},
+        duration: {type: Number},
+        weight: {type: Number},
+        reps: {type: Number},
+        sets: {type: Number},
+    }]
+});
+
+const workout = mongoose.model("Workouts", userWorkout);
+
+module.exports = workout;
